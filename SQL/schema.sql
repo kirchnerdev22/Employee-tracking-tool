23afe9282726aa132b4dfafeea1f67cdef596d99
kirchnerdev22 <trentonlkirchner@gmail.com>
2023-01-12 16:41:37 -0600

    fixed_schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -3,23 +3,29 @@ CREATE database employeeDatabase;
 USE employeeDatabase;
 
 CREATE TABLE department (
-    id: INT PRIMARY KEY,
-    dept_name: VARCHAR(30)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id: INT PRIMARY KEY,
-    title: VARCHAR(30),
-    salary: DECIMAL,
-    department_id: INT
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY(department_id) REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 
 CREATE TABLE employee (
-    id: INT PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT,
-    manager_id: NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
 );
 
--- SQL/seeds.sql
+++ SQL/seeds.sql
@@ -1,24 +1,38 @@
 USE employeeDatabase;
 
-INSERT INTO department (department_name)
+INSERT INTO department (dept_name)
 VALUES ("Revenue"),
-("Engineering"),
-("Management"),
-("Human resources");
+("Marketing"),
+("Human resources"),
+("Finance"),
+("Information Technology");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Revenue Lead", 100000, 1),
-("Front-end Engineer", 150000, 2),
-("Back-end Engineer", 120000, 3),
-("Manager", 125000, 4),
-("Insurance Director", 250000, 5);
+VALUES ("Sales Manager", 120000, 1),
+("Sales Representative", 70000, 1),
+("Marketing Manager", 120000, 2),
+("Marketing Coordinator", 60000, 2),
+("HR Manager", 120000, 3),
+("HR Generalist", 80000, 3),
+("Financial Manager", 150000, 4),
+("Financial Analyst", 90000, 4),
+("Systems Administrator", 90000, 5),
+("IT Support Technician", 60000, 5);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Doe", 1, 1),
-("John", "Wick", 2, 1),
-("Joe", "Biden", 3, 1),
-("Bill", "Nye", 4, 1),
-("Daisy", "Duke", 5, 1),
-("Mike", "Jones", 2, 1),
-("Tom", "Riddle", 4, 1),
-("Harry", "Potter", 1, 1);
+VALUES git add --a("Samantha", "Smith", 1, null),
+("Michael", "Johnson", 2, 1),
+("Joshua", "Williams", 2, 1),
+("Ashley", "Brown", 3, null),
+("Nicholas", "Garcia", 4, 4),
+("Jessica", "Davis", 4, 4),
+("Matthew", "Rodriguez", 5, null),
+("Brittany", "Miller", 6, 7),
+("Andrew", "Martinez", 6, 7),
+("Amanda", "Anderson", 7, null),
+("David", "Taylor", 8, 10),
+("Ryan", "Thomas", 8, 10),
+("Jonathan", "Hernandez", 9, null),
+("Rachel", "Moore", 10, 13),
+("Kevin", "Marten", 10, 13);
